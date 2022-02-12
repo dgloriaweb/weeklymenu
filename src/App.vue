d8f7bc335b5a7ffac16b672c8ae0dec2d1d8e095
unknown <dgloria.web@gmail.com>
2022-02-12 19:45:18 +0000

    added some main stuff

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,36 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <Welcome />
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import Welcome from "./components/Welcome.vue"
 
 export default {
   name: "App",
   components: {
-    HelloWorld,
+    Welcome,
   },
 };
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+body,
+html {
+  background-color: dark-grey;
+  color: #eee;
+  font-size: 16px;
+  font-family: "Open Sans", sans-serif;
+  background: #1d1f20;
+}
+
+#container {
+  width: 1000px;
+  max-width: 78vw;
+  background-color: grey;
+  margin: auto;
+}
+#banner{
+  background-color: teal;
+  min-height:10vh;
 }
 </style>
---
+++ src/components/Welcome.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <!-- header -->
+    <!-- hamburger -->
+    <div id="container">
+      <span class="hamburger"></span>
+      <h1>Weekly menu</h1>
+    </div>
+      <!-- some banner here -->
+      <div id="banner">Banner here</div>
+
+    <!-- content here   -->
+
+    <!-- footer -->
+    <div id="container">
+      <small> &copy;2022. by dgloria.web@gmail.com </small>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="css" scoped>
+.hamburger {
+  position: relative;
+  display: inline-block;
+  width: 1.25em;
+  height: 0.8em;
+  margin-right: 0.3em;
+  border-top: 0.2em solid #fff;
+  border-bottom: 0.2em solid #fff;
+}
+
+.hamburger:before {
+  content: "";
+  position: absolute;
+  top: 0.3em;
+  left: 0px;
+  width: 100%;
+  border-top: 0.2em solid #fff;
+}
+</style>
